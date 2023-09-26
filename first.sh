bb4df81fbf25e67f19e197236e94ba2a2bac090a
Ênrell <enrellsa10@proton.me>
2023-09-25 22:23:11 -0300

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if ! grep -q wheel /etc/group; then
-    echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
-fi
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/wheel
 echo -n "Enter your username: " && read username
 useradd -m -G wheel -s /bin/bash $username
 passwd $username
